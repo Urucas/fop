5203d8cd8faed3a43ed71e41a44f575485a5f41a
vrunoa <vruno.alassia@gmail.com>
2015-07-07 20:15:12 -0300

    add tests

---
+++ tests/test.js
@@ -0,0 +1,35 @@
+import fop from '../lib/';
+
+describe("Fop instance test", () => {
+
+  it("Test path -> /Users/vruno/myfile.txt", (done) => {
+    let folder = fop("/Users/vruno/myfile.txt");
+    if(folder != "/Users/vruno/") throw new Error("Fails, return folder:"+folder);
+    done();
+  });
+
+  it("Test path -> /Users/vruno/", (done) => {
+    let folder = fop("/Users/vruno/");
+    if(folder != "/Users/vruno/") throw new Error("Fails, return folder:"+folder);
+    done();
+  });
+
+  it("Test path -> myfile.txt", (done) => {
+    let folder = fop("myfile.txt");
+    if(folder != "/") throw new Error("Test fails, return folder:"+folder);
+    done();
+  });
+  
+  it("Test path -> /Users/vruno", (done) => {
+    let folder = fop("/Users/vruno");
+    if(folder != "/Users/") throw new Error("Test fails, return folder:"+folder);
+    done();
+  });
+  
+  it("Test path -> undefined", (done) => {
+    let folder = fop();
+    if(folder != "") throw new Error("Test fails, return folder:"+folder);
+    done();
+  });
+
+});
